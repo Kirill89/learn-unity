02579632c5afd9bdfa5ac9582e3b2456bc81007e
Kirill Efimov <kirill@snyk.io>
2020-04-15 18:52:27 +0300

    day 1

---
+++ day-1-clock/Assets/Camera.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform position;
+
+    const float Z_MIN = -20f;
+    const float Z_MAX = -10f;
+    const float STEP = 0.01f;
+
+    private bool zoomOut = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (zoomOut) {
+            position.localPosition = new Vector3(
+                position.localPosition.x,
+                position.localPosition.y,
+                position.localPosition.z - STEP
+            );
+            if (position.localPosition.z <= Z_MIN) {
+                zoomOut = !zoomOut;
+            }
+        } else {
+            position.localPosition = new Vector3(
+                position.localPosition.x,
+                position.localPosition.y,
+                position.localPosition.z + STEP
+            );
+            if (position.localPosition.z >= Z_MAX) {
+                zoomOut = !zoomOut;
+            }
+        }
+
+        // Debug.Log(position.localPosition);
+    }
+}
---
+++ day-1-clock/Assets/Clock.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    const float DEGREES_PER_HOUR = 30f;
+    const float DEGREES_PER_MINUTE = 6f;
+    const float DEGREES_PER_SECOND = 6f;
+
+    public bool continuous = false;
+    public Transform hours;
+    public Transform minutes;
+    public Transform seconds;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (continuous)
+        {
+            TimeSpan time = DateTime.Now.TimeOfDay;
+            hours.localRotation = Quaternion.Euler(0f, (float)time.TotalHours * DEGREES_PER_HOUR, 0f);
+            minutes.localRotation = Quaternion.Euler(0f, (float)time.TotalMinutes * DEGREES_PER_MINUTE, 0f);
+            seconds.localRotation = Quaternion.Euler(0f, (float)time.TotalSeconds * DEGREES_PER_SECOND, 0f);
+        }
+        else
+        {
+            DateTime time = DateTime.Now;
+            hours.localRotation = Quaternion.Euler(0f, time.Hour * DEGREES_PER_HOUR, 0f);
+            minutes.localRotation = Quaternion.Euler(0f, time.Minute * DEGREES_PER_MINUTE, 0f);
+            seconds.localRotation = Quaternion.Euler(0f, time.Second * DEGREES_PER_SECOND, 0f);
+        }
+    }
+}
