6cf0abdaa1d9d79ffb5dae2eca0e7127bd6f2c65
Kirill Efimov <kirill@snyk.io>
2020-04-30 10:17:51 +0300

    day 15: working on production-ready version of multi-scene (part 3)

--- circle-runner/Assets/Camera/Camera.cs
+++ circle-runner/Assets/Camera/Camera.cs
@@ -2,28 +2,61 @@
 
 public class Camera : MonoBehaviour
 {
+    public Transform levelBounds;
+
     private readonly Rect playerBounds = new Rect(-20f, -20f, 40f, 40f);
     private Player player;
+    private UnityEngine.Camera gameCamera;
+    private float levelWidth;
+    private float levelHeight;
 
     private void Start()
     {
         player = FindObjectOfType<Player>();
+        gameCamera = GetComponent<UnityEngine.Camera>();
+
+        levelWidth = levelBounds.localScale.x;
+        levelHeight = levelBounds.localScale.y;
     }
 
     private void Update()
     {
-        transform.localPosition = new Vector3(
-            player.transform.localPosition.x,
-            player.transform.localPosition.y,
-            transform.localPosition.z
-        );
-
+        UpdateCameraPosition();
         HandlePlayerFall();
+    }
+
+    private void UpdateCameraPosition()
+    {
+        var camHeight = 2f * gameCamera.orthographicSize;
+        var camWidth = camHeight * gameCamera.aspect;
+
+        var distX = 0f;
+        var distY = 0f;
+
+        if (camWidth < levelWidth)
+        {
+            distX = levelWidth - camWidth;
+        }
+
+        if (camHeight < levelHeight)
+        {
+            distY = levelHeight - camHeight;
+        }
+
+        var camX = Mathf.Clamp(player.transform.localPosition.x, levelBounds.localPosition.x - distX, levelBounds.localPosition.x + distX);
+        var camY = Mathf.Clamp(player.transform.localPosition.y, levelBounds.localPosition.y - distY, levelBounds.localPosition.y + distY);
+
+        transform.localPosition = new Vector3(camX, camY, transform.localPosition.z);
     }
 
     private void HandlePlayerFall()
     {
-        if (!playerBounds.Contains(player.transform.localPosition))
+        var violateMinX = player.transform.localPosition.x < levelBounds.localPosition.x - levelWidth / 2f;
+        var violateMinY = player.transform.localPosition.y < levelBounds.localPosition.y - levelHeight / 2f;
+        var violateMaxX = player.transform.localPosition.x > levelBounds.localPosition.x + levelWidth / 2f;
+        var violateMaxY = player.transform.localPosition.y > levelBounds.localPosition.y + levelHeight / 2f;
+
+        if (violateMinX || violateMinY || violateMaxX || violateMaxY)
         {
             Helpers.RestartLevel();
         }
